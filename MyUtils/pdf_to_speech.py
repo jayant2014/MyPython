d46b09a1342739ab2c79c34e012e87dd3b9fb880
Test User <testuser@gmail.com>
2020-09-30 11:06:30 +0530

    Pdf to speech utility

---
+++ MyUtils/pdf_to_speech.py
@@ -0,0 +1,13 @@
+import pyttsx3
+import PyPDF2
+book = open('Hooked_In_Real_Life_ebook.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(book)
+num_pages = pdf_reader.getNumPages()
+print("Number of pages in document : ", num_pages)
+
+speaker = pyttsx3.init()
+for index in range(1, num_pages):
+    page = pdf_reader.getPage(index)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
